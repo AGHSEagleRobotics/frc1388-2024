20525d179475fdbbf6d5a5589116672852266239
Jeff Techau <jtechau@gmail.com>
2024-02-01 19:26:06 -0800

    Added an import

--- frc1388-2024/src/main/java/frc/robot/Robot.java
+++ frc1388-2024/src/main/java/frc/robot/Robot.java
@@ -9,6 +9,7 @@ import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.RobotController.RadioLEDState;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
