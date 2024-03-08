6b54babe8e1b319f09baefef6383747491895d02
Calvin Richards <richardscalvin119@gmail.com>
2024-03-08 11:23:16 -0800

    fixing led subsystem

--- frc1388-2024/src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ frc1388-2024/src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -13,21 +13,21 @@ import frc.robot.Constants.LEDConstants;
 public class LEDSubsystem extends SubsystemBase {
 
   private final PWMSparkMax m_led;
-  private final boolean m_isOnRed;
+  // private final boolean m_isOnRed;
 
   /** Creates a new LEDSubsystem. */
   public LEDSubsystem(PWMSparkMax led) {
     m_led = led;
-    m_isOnRed = (DriverStation.getAlliance().get() == Alliance.Red);
+    // m_isOnRed = (DriverStation.getAlliance().get() == Alliance.Red);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    if (m_isOnRed) {
-      m_led.set(LEDConstants.RED_SOLID);
-    } else {
-      m_led.set(LEDConstants.BLUE_SOLID);
-    }
+    // if (m_isOnRed) {
+    //   m_led.set(LEDConstants.RED_SOLID);
+    // } else {
+    //   m_led.set(LEDConstants.BLUE_SOLID);
+    // }
   }
 }
