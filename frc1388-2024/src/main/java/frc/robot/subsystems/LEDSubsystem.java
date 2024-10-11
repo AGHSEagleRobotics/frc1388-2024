dfc9fe8ee4fb54e28d2d46bd1cd647a7c8fbb982
Team1388 <programming@eaglerobotics.com>
2024-10-10 20:52:45 -0700

    Internal LEDs - One row shows alliance color - Other row is a battery meter
    The battery voltage isn't reporting accurately from the CANdle.

--- frc1388-2024/src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ frc1388-2024/src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -22,27 +22,82 @@ public class LEDSubsystem extends SubsystemBase {
 
   private final CANdle m_candle; // CANdle canid is 42
   // private final boolean m_isOnRed;
+  private int m_loopCount = 0;
 
-  /** Creates a new LEDSubsystem. */  
+  private final int LOOPS_PER_SECOND = 50;
+
+  /** Creates a new LEDSubsystem. */
   public LEDSubsystem(CANdle candle) {
     m_candle = candle;
     // m_isOnRed = (DriverStation.getAlliance().get() == Alliance.Red);
-      CANdleConfiguration config = new CANdleConfiguration();
-      config.stripType = LEDStripType.RGB;
-      config.brightnessScalar = 0.3; // dim the LEDs to half brightness
-      m_candle.configAllSettings(config);
+    CANdleConfiguration config = new CANdleConfiguration();
+    config.stripType = LEDStripType.RGB;
+    config.brightnessScalar = 0.1; // dim the LEDs to half brightness
+    m_candle.configAllSettings(config);
 
-    candle.setLEDs(0, 0, 0);
-    candle.setLEDs(255, 0, 255, 0, 2, 1);
-    candle.setLEDs(0, 255, 255, 0, 5, 1);
+    LED_reset();
+  }
 
-}
+  /** Reset internal and external LEDs - turn off LEDs and animations */
+  public void LED_reset() {
+    // Turn off LEDs
+    m_candle.setLEDs(0, 0, 0);
 
+    // clear all animations
+    for (int slot = 0; slot < m_candle.getMaxSimultaneousAnimationCount(); slot++) {
+      m_candle.clearAnimation(slot);
+    }
+  }
 
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    m_loopCount++;
+
+    // Once per second:
+    if ((m_loopCount % LOOPS_PER_SECOND) == 0) {
+
+      // set alliance color
+      Alliance alliance_color = DriverStation.getAlliance().get();
+      if (alliance_color == Alliance.Red) {
+        m_candle.setLEDs(255, 0, 0, 0, 4, 4);
+      }
+      else if (alliance_color == Alliance.Blue) {
+        m_candle.setLEDs(0, 0, 255, 0, 4, 4);
+      }
+      else {
+        m_candle.setLEDs(0, 0, 0, 0, 4, 4);
+      }
+    }
+    
+    // Battery meter
+    {
+      final double VBAT_CRITICAL = 12.2;
+      final double VBAT_LOW = 12.4;
+      final double VBAT_OK = 12.6;
+      
+      // Set LEDs
+      double vBat = m_candle.getBusVoltage();
+      System.out.println(vBat);
+      if (vBat < VBAT_CRITICAL) {
+        m_candle.setLEDs(255, 0,   0, 0, 0, 1);
+        m_candle.setLEDs(0,   0,   0, 0, 1, 3);
+      }
+      else if (vBat < VBAT_LOW) {
+        m_candle.setLEDs(255, 255, 0, 0, 0, 2);
+        m_candle.setLEDs(0,   0,   0, 0, 2, 2);
+      }
+      else if (vBat < VBAT_OK) {
+        m_candle.setLEDs(0,   255, 0, 0, 0, 3);
+        m_candle.setLEDs(0,   0,   0, 0, 3, 1);
+      }
+      else if (vBat < VBAT_OK) {
+        m_candle.setLEDs(0,   255, 0, 0, 0, 4);
+      }
+      
+    }
+
     // if (m_isOnRed) {
     //   m_led.set(LEDConstants.RED_SOLID);
     // } else {
